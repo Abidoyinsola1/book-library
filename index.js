e07b0b24bf8b3efa73149afd4ef85a1685d08a61
Abiola Doyinsola <ifeobayemi@gmail.com>
2021-01-19 16:30:39 +0100

    add column

--- index.js
+++ index.js
@@ -1,84 +1,65 @@
-const booksList = document.querySelector('#books')
+const booksList = document.querySelector("#books");
 
 let myLibrary = [];
 
-function Book(author, title, pages ) {
-  this.author = author
-  this.title = title
-  this.pages = pages
+function Book(author, title, pages) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
 }
 
-
-function clearField(){
-    document.querySelector('#title').value = ''
-    document.querySelector('#author').value = ''
-    document.querySelector('#pages').value = ''
+function clearField() {
+  document.querySelector("#title").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#pages").value = "";
 }
 
+function addBook(e) {
+  e.preventDefault();
 
-function addBook(e){
-    e.preventDefault();  
+  const [title, author, pages] = e.target.elements;
+  const book = new Book(author.value, title.value, pages.value);
 
-   const [title, author, pages] = e.target.elements
-   const book = new Book(author.value, title.value, pages.value); 
-    
-    
-    // creating list of books
-    //const li = document.createElement('li')
-    //li.className = 'book-list';
+  const div = document.createElement("div");
 
-    //li.appendChild(document.createTextNode([author.value, title.value, pages.value]))
-
-    const div = document.createElement('div');
-
-    div.innerHTML = `
-
+  div.innerHTML = `
+<div class="row">
+<div class="col-md-3">
     <h5 class="card-title mb-0"> ${book.title} </h5>
     <p class="card-text">Author: <span id="author">${book.author}</span></p>
-    <p class="card-text">Pages: <span id="pages">${book.pages}</span></p>
+    <p class="card-text">Pages: <span id="pages">${book.pages}</span></p></div></div>
  `;
 
+  // delete icon
+  const link = document.createElement("a");
+  link.className = "delete-book";
+  link.innerHTML = '<i class="fas fa-trash-alt delete"></i>';
 
+  // add delete icon to li
+  booksList.appendChild(link);
+  booksList.appendChild(div);
 
-    // delete icon
-    const link = document.createElement('a')
-    link.className = 'delete-book';
-    link.innerHTML = '<i class="fas fa-trash-alt delete"></i>'
-    
-    // add delete icon to li
-    booksList.appendChild(link)
-    booksList.appendChild(div)
+  // add li to the ul
+  //booksList.appendChild(li)
 
+  //console.log(booksList)
 
-    // add li to the ul
-    //booksList.appendChild(li)
+  myLibrary.push([author.value, title.value, pages.value]);
 
-    //console.log(booksList)
-
-    myLibrary.push([author.value, title.value, pages.value])
-
-    clearField()
-  
+  clearField();
 }
-
-
 
 function loadEventListeners() {
-  document.querySelector('#form').addEventListener('submit', addBook)
-  booksList.addEventListener('click', removeBook);
-  
+  document.querySelector("#form").addEventListener("submit", addBook);
+  booksList.addEventListener("click", removeBook);
 }
 
-
 function removeBook(e) {
-  if(e.target.parentElement.classList.contains('delete-book')) {
-    if(confirm('Are You Sure?')) {
+  if (e.target.parentElement.classList.contains("delete-book")) {
+    if (confirm("Are You Sure?")) {
       e.target.parentElement.parentElement.remove();
-
     }
   }
 }
 
-
 loadEventListeners();
-
