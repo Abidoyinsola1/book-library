28d2b7d52026e55af41078cdd41bc4ca9075635f
Abiola Doyinsola <ifeobayemi@gmail.com>
2021-01-22 09:14:14 +0100

    Fix ESLint errors

--- index.js
+++ index.js
@@ -60,19 +60,18 @@ function loadEventListeners() {
 
 let a;
 
-function hideShow(){
-  if(a==1){
-     document.getElementById('author').style.display='block'
-     document.getElementById('title').style.display='block'
-     document.getElementById('pages').style.display='block'
-     return a=0;
+function hideShow() {
+  if (a === 1) {
+    document.getElementById('author').style.display = 'block';
+    document.getElementById('title').style.display = 'block';
+    document.getElementById('pages').style.display = 'block';
+    return a = 0;
   }
-  else{
-    document.getElementById('author').style.display='none'
-    document.getElementById('title').style.display='none'
-    document.getElementById('pages').style.display='none'
-    return a=1
-  }
+
+  document.getElementById('author').style.display = 'none';
+  document.getElementById('title').style.display = 'none';
+  document.getElementById('pages').style.display = 'none';
+  return a = 1;
 }
 
 loadEventListeners();
